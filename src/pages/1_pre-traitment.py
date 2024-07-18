30125c1cfb0d9b3c9e0959d8a0d89bbfc15391b6
juleroule <jule.roule@efrei.net>
2024-07-18 08:16:00 +0200

    update with pages

--- src/pages/1_pre-traitment.py
+++ src/pages/1_pre-traitment.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from data_loading import load_data, display_data_preview, display_data_summary
+from data_cleaning import handle_missing_values, normalize_data, encode_categorical_columns
 import os
 
 def local_css(file_name):
@@ -13,28 +13,32 @@ def local_css(file_name):
 
 def main():
     # Chargement du CSS local
-    local_css("styles.css")
+    local_css("../styles.css")
 
     # Ajout du logo dans la barre latérale
     current_dir = os.path.dirname(__file__)
-    logo_path = os.path.join(current_dir, "image.png")
+    logo_path = os.path.join(current_dir, "../image.png")
     if os.path.exists(logo_path):
         st.sidebar.image(logo_path, use_column_width=True)
     else:
         st.sidebar.error("Logo non trouvé.")
 
-    st.header("Projet Data Mining")
+    st.header("Pré-traitement et nettoyage des données")
     if 'data' not in st.session_state:
         st.session_state.data = None
-    
-    data = load_data()
-    if data is not None:
-        st.session_state.data = data
-        display_data_preview(data)
-        display_data_summary(data)
-    elif st.session_state.data is not None:
-        display_data_preview(st.session_state.data)
-        display_data_summary(st.session_state.data)
+
+    if st.session_state.data is not None:
+        data = handle_missing_values(st.session_state.data)
+        if data is not None:
+            data = encode_categorical_columns(data)
+        if data is not None:
+            data = normalize_data(data)
+        if data is not None:
+            st.session_state.data = data
+        else:
+            st.warning("Le pré-traitement des données a échoué. Veuillez vérifier votre dataset et réessayer.")
+    else:
+        st.warning("Veuillez d'abord charger les données.")
 
 if __name__ == "__main__":
     main()
